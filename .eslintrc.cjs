a90bd88aeab6e9c8b0d2ed6cc396f2d05f58a67a
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-01-31 08:03:13 +0100

    modified eslint file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,19 @@
 module.exports = {
   root: true,
+  env: { browser: true, es2020: true },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
+    // tsconfigRootDir: __dirname,
   },
-
-  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
-    "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
